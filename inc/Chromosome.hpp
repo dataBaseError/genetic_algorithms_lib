937c53c59aa6780c03730977e8b09e697259c173
Joseph <heron.joseph@gmail.com>
2014-11-01 23:26:15 -0400

    ADDED: genetic operations

--- inc/Chromosome.hpp
+++ inc/Chromosome.hpp
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <vector>
+#include <algorithm>
 
 template <class T>
 class Chromosome {
@@ -18,23 +19,59 @@ public:
 
     }
 
-    void mutate(double mutation_rate) {
+    void initialize() {
+    	// Initialize the chromosome.
+    }
+
+    void mutate(double mutation_rate, std::vector<Chromosome<T > > &children, T min_chromosome_value, T max_chromosome_value) {
+    	clonning(children);
+
+    	// Identify element that will be changed
+    	int mutated_index = getRandomElement();
+
     	// Apply mutation operation to the chromosome
+    	if(std::is_same<T, bool>::value) {
+    		// Flip the bit
+    		children.back()[mutated_index] != children.back()[mutated_index];
+    	} else {
+    		// Choose a random number within the range
+    		children.back()[mutated_index] = rand() % max_chromosome_value + min_chromosome_value;
+    	}
     }
 
-    void crossover(double crossover_rate, Chromosome<T > &other) {
+    void crossover(double crossover_rate, Chromosome<T > &other, std::vector<Chromosome<T > > &children) {
+    	// Copy value to the next population
+    	clonning(children);
+		// Copy other to the next population
+    	clonning(children, other);
+
+    	// Randomly pick one point (where the cross over starts)
+    	int crossover_index = getRandomElement();
+
     	// Apply crossover operation to the chromosome
+
+    	// Using the index inclusive approach
+    	// First gets l1 + r2 and second gets l2 + r1
+    	std::swap_ranges((*(children.end()-2)).begin(), (*(children.end()-2)).begin()+crossover_index, (*(children.end()-1)).rbegin()+crossover_index+1);
+
+    	// Alternative exclusive index approach
+    	//std::swap_ranges((*(children.end()-2)).begin(), (*(children.end()-2)).begin()+crossover_index+1, (*(children.end()-1)).rbegin()+crossover_index);
+
     }
 
-    //void
-
-    /*T operator[] (const unsigned int i) {
-    	return this->chromosomes[i];
+    void clonning(std::vector<Chromosome<T > > &children) {
+    	// Copy value to the next population
+    	children.push_back(chromosome);
     }
 
-    void set (const unsigned int i, T value) {
-    	this->chromosomes[i] = value;
-    }*/
+    void clonning(std::vector<Chromosome<T > > &children, Chromosome<T > &chromosome) {
+		// Copy value to the next population
+		children.push_back(chromosome);
+	}
+
+    int getRandomElement() {
+    	return rand() % chromosome.size()-1;
+    }
 
 };
 
--- src/Chromosome.cpp
+++ src/Chromosome.cpp
@@ -1,23 +1,2 @@
 #include "Chromosome.hpp"
 
-void init() {
-    srand(time(NULL));
-    
-}
-
-void mutate(chromosome& chromo) {
-    // Apply the mutation operation
-}
-
-void crossover(chromosome& chromo1, chromosome& chromo2) {
-
-}
-
-void inbreeding(bool inbreed) {
-    int r = min_chromosome_value + (int)(max_chromosome_value * rand() / (RAND_MAX + 1.0));
-    //1 + (int)( 10.0 * rand() / ( RAND_MAX + 1.0 ) )
-}
-
-void calcSimilarity(chromosome* chromo) {
-    
-}
