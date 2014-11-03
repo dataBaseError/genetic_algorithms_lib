87583f3989300eac1466db874459a6fd9ef44e6d
gnu_user <gsoc.student@gmail.com>
2014-11-03 16:32:14 -0500

    Added the initial roulette wheel selection method

--- inc/RouletteWheel.hpp
+++ inc/RouletteWheel.hpp
@@ -1,18 +1,28 @@
-/*
- * RouletteWheel.hpp
- *
- *  Created on: Nov 3, 2014
- *      Author: jon
- */
 
 #ifndef ROULETTEWHEEL_HPP_
 #define ROULETTEWHEEL_HPP_
 
+#include <random>
+#include <map>
 #include "Selection.hpp"
 
 class RouletteWheel : public Selection
 {
+private:
+    mt19937 engine;
+    uniform_real_distribution<double> distribution;
+    const double EPSILON = 1.0E-15;
+    double left = 0.0;
+    double right = 0.0 + EPSILON;
+    map<pair<double, double>, SimpleChromosome> selection;
+
 public:
+    /**
+     * Default constructor for roulette wheel selection method, initializes
+     * the random engine used for selection process.
+     */
+    RouletteWheel();
+
     /**
      * Initialize the selection iterator with the fitness distribution of the
      * chromosomes which is used to generate the selection range of chromosomes
--- src/RouletteWheel.cpp
+++ src/RouletteWheel.cpp
@@ -1,7 +1,46 @@
 #include "../inc/RouletteWheel.hpp"
 
-void RouletteWheel::init(vector<pair<SimpleChromosome, double> > &fitness)
+RouletteWheel::RouletteWheel()
 {
-    return;
+    random_device rd;
+    this->engine = mt19937(rd());
 }
 
+void RouletteWheel::init(vector<pair<SimpleChromosome, double> > &fitness)
+{
+    double lower = this->left;
+    double upper = this->right;
+
+    // Calculate the right bound
+    for (vector<pair<SimpleChromosome, double> >::iterator it = fitness.begin(); it != fitness.end(); ++it)
+    {
+        this->right += it->second + this->EPSILON;
+    }
+
+    // Initialize the uniform distribution random generator
+    this->distribution = uniform_real_distribution<double>(this->left, this->right);
+
+    // Add each chromosome and interval to roulette wheel selection
+    for (vector<pair<SimpleChromosome, double> >::iterator it = fitness.begin(); it != fitness.end(); ++it)
+    {
+        upper = lower + it->second;
+        this->selection.insert({make_pair(lower, upper), it->first});
+        lower = upper + this->EPSILON;
+    }
+}
+
+SimpleChromosome RouletteWheel::next()
+{
+    double rand_num = this->distribution(this->engine);
+
+    // Find the chromosome where the random number is in the interval
+    for (map<pair<double, double>, SimpleChromosome>::iterator it = this->selection.begin(); it != this->selection.end(); ++it)
+    {
+        if (rand_num >= it->first.first && rand_num < it->first.second)
+        {
+            return it->second;
+        }
+    }
+
+    // TODO fatal error due to floating point arithmetic if no match found!
+}
