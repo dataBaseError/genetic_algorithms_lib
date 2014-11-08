a2f42e4f16abbd7bbb948db4f32184a1cdb97cd0
Joseph <heron.joseph@gmail.com>
2014-11-08 11:25:50 -0500

    REFACTORED: header code into cpp file

--- inc/RouletteWheel.hpp
+++ inc/RouletteWheel.hpp
@@ -6,35 +6,25 @@
 #include <map>
 
 #include "Selection.hpp"
-//#include "Chromosome.hpp"
-
-using namespace std;
 
 class RouletteWheel : public Selection
 {
 private:
-    mt19937 engine;
-    uniform_real_distribution<double> distribution;
+    std::mt19937 engine;
+    std::uniform_real_distribution<double> distribution;
     const double EPSILON = 1.0E-15;
     double left = 0.0;
     double right = 0.0 + EPSILON;
-    map<pair<double, double>, unsigned int > selection;
+    std::map<std::pair<double, double>, unsigned int > selection;
 
 public:
     /**
      * Default constructor for roulette wheel selection method, initializes
      * the random engine used for selection process.
      */
-    RouletteWheel()
-	{
-		random_device rd;
-		this->engine = mt19937(rd());
-	}
+    RouletteWheel();
 
-    virtual ~RouletteWheel()
-	{
-
-	}
+    virtual ~RouletteWheel();
 
     /**
      * Initialize the selection iterator with the fitness distribution of the
@@ -44,28 +34,7 @@ public:
      * @param fitness The fitness distribution for the chromosomes to be used
      * for the roulette wheel selection.
      */
-    virtual void init(std::vector<std::pair<unsigned int, double> > &fitness)
-    {
-    	double lower = this->left;
-    	double upper = this->right;
-
-    	// Calculate the right bound
-    	for (auto it = fitness.begin(); it != fitness.end(); ++it)
-    	{
-    		this->right += it->second + this->EPSILON;
-    	}
-
-    	// Initialize the uniform distribution random generator
-    	this->distribution = uniform_real_distribution<double>(this->left, this->right);
-
-    	// Add each chromosome and interval to roulette wheel selection
-    	for (auto it = fitness.begin(); it != fitness.end(); ++it)
-    	{
-    		upper = lower + it->second;
-    		this->selection.insert({make_pair(lower, upper), it->first});
-    		lower = upper + this->EPSILON;
-    	}
-    }
+    virtual void init(std::vector<std::pair<unsigned int, double> > &fitness);
 
     /**
      * The iterator method which uses roulette wheel selection to get the next
@@ -73,23 +42,7 @@ public:
      *
      * @return The next chromosome selected.
      */
-    virtual unsigned int next()
-	{
-		double rand_num = this->distribution(this->engine);
-
-		// Find the chromosome where the random number is in the interval
-		for (auto it = this->selection.begin(); it != this->selection.end(); ++it)
-		{
-			if (rand_num >= it->first.first && rand_num < it->first.second)
-			{
-				return it->second;
-			}
-		}
-
-		// TODO fatal error due to floating point arithmetic if no match found!
-		//unsigned int bad_return = 0;
-		return 0;
-	}
+    virtual unsigned int next();
 };
 
 #endif /* ROULETTEWHEEL_HPP_ */
--- src/RouletteWheel.cpp
+++ src/RouletteWheel.cpp
@@ -1,40 +1,40 @@
-#include "../inc/RouletteWheel.hpp"
+#include "RouletteWheel.hpp"
 
-/*
+
 RouletteWheel::RouletteWheel()
 {
-    random_device rd;
-    this->engine = mt19937(rd());
-}*/
+    std::random_device rd;
+    this->engine = std::mt19937(rd());
+}
 
-/*template<class T >
-void RouletteWheel::init(vector<pair<Chromosome<T >, double> > &fitness)
+RouletteWheel::~RouletteWheel() {
+
+}
+
+void RouletteWheel::init(std::vector<std::pair<unsigned int, double> > &fitness)
 {
 	double lower = this->left;
 	double upper = this->right;
 
 	// Calculate the right bound
-
 	for (auto it = fitness.begin(); it != fitness.end(); ++it)
 	{
 		this->right += it->second + this->EPSILON;
 	}
 
 	// Initialize the uniform distribution random generator
-	this->distribution = uniform_real_distribution<double>(this->left, this->right);
+	this->distribution = std::uniform_real_distribution<double>(this->left, this->right);
 
 	// Add each chromosome and interval to roulette wheel selection
 	for (auto it = fitness.begin(); it != fitness.end(); ++it)
 	{
 		upper = lower + it->second;
-		this->selection.insert({make_pair(lower, upper), it->first});
+		this->selection.insert({std::make_pair(lower, upper), it->first});
 		lower = upper + this->EPSILON;
 	}
-}*/
+}
 
-/*
-template<class T >
-Chromosome<T > RouletteWheel::next()
+unsigned int RouletteWheel::next()
 {
 	double rand_num = this->distribution(this->engine);
 
@@ -48,4 +48,6 @@ Chromosome<T > RouletteWheel::next()
 	}
 
 	// TODO fatal error due to floating point arithmetic if no match found!
-}*/
+	//unsigned int bad_return = 0;
+	return 0;
+}
