e78a16a81c3515ef3c41792d2811415cebb02dc9
Joseph <heron.joseph@gmail.com>
2014-12-09 01:31:04 -0500

    ADDED: documentation for new queue methods.

--- inc/SafeQueue.hpp
+++ inc/SafeQueue.hpp
@@ -70,17 +70,26 @@ public:
 		return pop();
 	}*/
 
-	// Condensed front and pop into a single method
-	// Should return NULL when queue is empty and this method is called
+	/**
+	 * Retrieves the next value from the queue. The value is also removed.
+	 * If the wait is to true the thread will sleep until the next value is
+	 * available.
+	 * 
+	 * @param result The next value in the queue.
+	 * @param wait Whether the pop will wait for a value if the queue is empty.
+	 * Default is to wait for the a value if the queue is empty.
+	 * 
+	 * @return Whether the next value was retrieved or not.
+	 *
+	 */
 	bool pop(T& result, bool wait=true) {
 
-		//sem_.wait();
-		//pthread_mutex_lock(&access_lock);
 		boost::unique_lock<boost::mutex> lock(mtx_);
 
 		while (values.size()==0 && !stop_waiting) {
 
 			if(!wait) {
+				// Queue is empty.
 				return false;
 			}
 
@@ -99,6 +108,21 @@ public:
 		return true;
 	}
 
+	/**
+	 * Retrieve the next amount of items from the queue. If the queue does not
+	 * have enough values the thread will wait.
+	 * 
+	 * This method can be potentially dangerous since values may be left on
+	 * the queue and never worked on if the the amount left on is less than the
+	 * desired amount
+	 * 
+	 * @param amount The number of items to retrieve from the front the queue.
+	 * @param results The output vector of the values retrieved.
+	 * 
+	 * @return Returns true if the number of values are within the results
+	 * vector and false otherwise.
+	 *
+	 */
 	bool pop(unsigned int amount, std::vector<T > &results) {
 		boost::unique_lock<boost::mutex> lock(mtx_);
 
@@ -120,6 +144,17 @@ public:
 		return true;
 	}
 
+	/**
+	 * Retrieves the next amount of items or less from the queue. If the queue
+	 * is empty the thread will wait. Otherwise up to the amount of elements
+	 * will be returned
+	 * 
+	 * @param amount The number of items to retrieve from the front the queue.
+	 * @param results The output vector of the values retrieved.
+	 * 
+	 * @return Returns true if some number of values are within the results
+	 * vector and false otherwise.
+	 */
 	bool popUpTO(unsigned int amount, std::vector<T > &results) {
 		boost::unique_lock<boost::mutex> lock(mtx_);
 
@@ -141,6 +176,12 @@ public:
 		return true;
 	}
 
+	/**
+	 * Simple push of a single value into the queue.
+	 *
+	 * @param The value to add to the queue.
+	 *
+	 */
 	void push(T &val) {
 		//pthread_mutex_lock(&access_lock);
 		boost::unique_lock<boost::mutex> guard(mtx_);
@@ -153,15 +194,29 @@ public:
 		//sem_post(&access_resource);
 	}
 
+	/**
+	 * Push a set of values into the queue.
+	 * This is implemented to increase performance for a push that involves a
+	 * large sum of entries which can be accomplished faster than fighting
+	 * other threads for access to the mutex several times.
+	 * 
+	 * @param entries The list of values to push into the queue.
+	 */
 	void push(std::vector<T > &entries) {
 		boost::unique_lock<boost::mutex> guard(mtx_);
 
+		
 		for(unsigned int i = 0; i < entries.size(); i++) {
 			values.push(entries[i]);
 			m_cond.notify_one();
 		}
 	}
 
+	/**
+	 * Notify all waiting threads to wake up and return false. This method
+	 * essentially is a nice way of stopping the threads that are waiting for
+	 * pop.
+	 */
 	void finish() {
 		
 		boost::unique_lock<boost::mutex> lock(mtx_);
