a3c54389364c9101b6eb40098365d6ae09fce349
Joseph <heron.joseph@gmail.com>
2014-12-09 12:02:49 -0500

    UPDATED: Results class
    Added accessor and mutator methods

--- inc/Result.hpp
+++ inc/Result.hpp
@@ -1,29 +1,27 @@
-/*
- * Result.hpp
- *
- *  Created on: Nov 21, 2014
- *      Author: joseph
- */
-
 #ifndef INC_RESULT_HPP_
 #define INC_RESULT_HPP_
 
 #include <vector>
 
 class Result {
-public:
+
 	unsigned int index;
 	double result;
 
-	Result() {
-		this->index = 0;
-		this->result = 0;
-	}
+public:
+	
 
-	Result(unsigned int index, double result) {
-		this->index = index;
-		this->result = result;
-	}
+	Result();
+
+	Result(unsigned int index, double result);
+
+	unsigned int getIndex();
+
+	void setIndex(unsigned int index);
+
+	double getResult();
+
+	void setResult(double result);
 
 };
 
